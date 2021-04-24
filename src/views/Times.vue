d9c84efda33b75bc77c3c865968184de41682213
Nicklas Wiegandt <nicklas@wiegandt.eu>
2021-04-24 21:46:00 +0200

    Changing some styling and adding mock data

--- src/views/Times.vue
+++ src/views/Times.vue
@@ -15,12 +15,11 @@
             </ion-col>
           </ion-row>
         </ion-grid>
-        
       </ion-toolbar>
     </ion-header>
-    
-  <ion-content :fullscreen="true">
-    <!--  
+
+    <ion-content :fullscreen="true">
+      <!--  
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-grid>
@@ -37,9 +36,16 @@
       </ion-header> -->
       <ion-list>
         <ion-item-group v-bind:key="month.name" v-for="month in loadMonths()">
-          <ion-item-divider color="light" sticky>{{month.name}} {{month.year}}</ion-item-divider>
+          <ion-item-divider color="light" sticky
+            >{{ month.name }} {{ month.year }}</ion-item-divider
+          >
           <ion-item v-for="day in month.days" v-bind:key="day.date">
-            <ion-label>{{day.weekday}} {{day.date}}</ion-label>
+            <ion-label color="medium">
+              <div>{{ day.weekday }}.</div>
+              <div class="bigger">
+                {{ day.date }}
+              </div>
+            </ion-label>
           </ion-item>
         </ion-item-group>
       </ion-list>
@@ -48,37 +54,69 @@
 </template>
 
 <script lang="ts">
-import { IonButtons, IonHeader, IonMenuButton, IonPage, IonToolbar, IonGrid, IonRow, IonCol, IonContent, IonList, IonItem } from '@ionic/vue';
+import {
+  IonButtons,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonToolbar,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonContent,
+  IonList,
+  IonItem,
+} from "@ionic/vue";
 
-const monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
-];
-const weekdayNames = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" ];
+import * as moment from 'moment';
 
-function daysInMonth (month: number, year: number): number {
-    return new Date(year, month, 0).getDate();
+function daysInMonth(month: number, year: number): number {
+  return new Date(year, month, 0).getDate();
 }
 
 interface Month {
   name: string;
   year: number;
-  days: Record<string, any>[];
+  days: Day[];
+}
+
+interface Day {
+  weekday: string;
+  date: string;
+  hasEntry: boolean;
+}
+
+interface Entry {
+  overtime: moment.Duration;
+  start: Date;
+  end: Date;
+  fullDay: boolean;
+  type: EntryType;
+}
+
+enum EntryType {
+  OVERTIME, WORK, VACATION
 }
 
 export default {
-  name: 'Zeiten',
+  name: "Zeiten",
   components: {
     IonButtons,
     IonHeader,
     IonMenuButton,
     IonPage,
     IonToolbar,
-    IonGrid, IonRow, IonCol, IonContent, IonList, IonItem
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonContent,
+    IonList,
+    IonItem,
   },
   data() {
     return {
-      title: 'Zeiten'
-    }
+      title: "Zeiten",
+    };
   },
   methods: {
     calcOvertime() {
@@ -89,26 +127,43 @@ export default {
     },
     loadMonths(): Month[] {
       const months: Month[] = [];
+      const mockDataEntries: Entry[] = [
+        {
+          overtime: moment.Duration()
+        },
+      ];
 
-      for(let monthModifier = 0; monthModifier < 12; monthModifier++)
-      {
+      for (let monthModifier = 0; monthModifier < 12; monthModifier++) {
         const baseDate = new Date();
-        baseDate.setMonth(new Date().getMonth()-monthModifier)
+        baseDate.setMonth(new Date().getMonth() - monthModifier);
         const month = baseDate.getMonth();
         const year = baseDate.getFullYear();
-        const currentMonth: Month = 
-        {
-          name: monthNames[month],
-          year: baseDate.getFullYear(),
-          days: []
+
+        const onlyMonthNameOptions: Intl.DateTimeFormatOptions = {
+          month: "long",
         };
-        for(let day = 1; day < daysInMonth(month, year); day++)
-        {
-          const options: Intl.DateTimeFormatOptions = { weekday: 'short', month: 'long', day: 'numeric' };
-          const newDay =  {
-              weekday: new Date(year,month, day).toLocaleDateString('de-DE', options),
-              date: ""
-            };
+        const currentMonth: Month = {
+          name: new Date(year, month).toLocaleDateString(
+            navigator.language,
+            onlyMonthNameOptions
+          ),
+          year: baseDate.getFullYear(),
+          days: [],
+        };
+        for (let day = 1; day < daysInMonth(month, year); day++) {
+          const date = new Date(year, month, day);
+          const onlyWeekDayOptions: Intl.DateTimeFormatOptions = {
+            weekday: "short",
+          };
+          const onlyDayOptions: Intl.DateTimeFormatOptions = { day: "numeric" };
+
+          const newDay: Day = {
+            weekday: date.toLocaleDateString(
+              navigator.language,
+              onlyWeekDayOptions
+            ),
+            date: date.toLocaleDateString(navigator.language, onlyDayOptions),
+          };
 
           currentMonth.days.push(newDay);
         }
@@ -116,13 +171,16 @@ export default {
       }
 
       return months;
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style scoped>
 .bold {
   font-weight: bold;
+}
+.bigger {
+  font-size: 1.25em;
 }
 </style>
